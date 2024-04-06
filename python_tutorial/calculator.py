7806b7440c1f0959d4f3f1d244b3fb70ef789463
Faith-Jackson <undisputedfaith@gmail.com>
2024-04-06 17:43:10 +0100

    done

--- python_tutorial/calculator.py
+++ python_tutorial/calculator.py
@@ -1,22 +1,22 @@
-#!/bin/env python
-
-num1, operator, num2 = input('Input the two numbers you want to calculate:\n').split()
+num1, placement, num2 = input("Please input two numbers that you want to calculate.\nPlease use the following signs x,-,%,/,+\n:").split()
 
 num1 = int(num1)
 num2 = int(num2)
 
-if operator == '+':
-	print(f"{num1} plus {num2} is equal to {num1+num2}")
+if placement == 'x':
+    print(f"{num1} multiplied by {num2} is equal to {num1*num2}")
 
-elif operator == '-':
-	print(f"{num1} minus {num2} is equal to {num1-num2}")
+elif placement == '+':
+    print(f"{num1} plus {num2} is equal to {num1+num2}")
 
-elif operator == '*':
-	print(f"{num1} times {num2} is equal to {num1*num2}")
+elif placement == '-':
+    print(f"{num1} minus {num2} is equal to {num1-num2}")
 
-elif operator == '/':
-	print(f"{num1} divided by {num2} is equal to {num1/num2}")
+elif placement == '/':
+    print(f"{num1} divided by {num2} is equal to {num1/num2}")
 
-elif operator == '%':
-	print(f"{num1} percent of {num2} is equal to {num1%num2}")
+elif placement == '%':
+    print(f"{num1} percent of {num2} is equal to {num1%num2}")
 
+else:
+    print("You have not used any valid placement")
--- python_tutorial/loop.py
+++ python_tutorial/loop.py
@@ -1,5 +1,15 @@
-#!/bin/env python
-num = 1
-while num <= 10:
-    num = num + 1
-    print(num)
+investment = input('Please enter your investment amount:\n')
+interest = input('Please input your stipulated interest:\n')
+
+investment = int(investment)
+interest = float(interest)
+
+earning = investment % interest
+
+earning = float(earning)
+
+for e in range(1, 10):
+    yearly = e*earning
+    print(f'Your interest for year {e} will be  {yearly:.2f}')
+tenyears = earning*10
+print(f'Your earning in 10 years will be {tenyears:.2f}')
